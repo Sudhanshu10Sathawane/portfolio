000bcc56b33f5eeefd4a7c1113ca071e9ac42e33
sudhanshu10sathawane <sudhanshusathwane@gmail.com>
2023-04-25 01:14:57 +0530

    Added Skills

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className='text-gray-400 mx-auto mx-w-[1240px] flex '>
-        <h1 className='w-full p-4 text-4xl hover:text-[#00df9a] font-bold font-signature border-b border-b-green-500'>Sudhanshu Sathawane</h1>
+        <h1 className='w-full p-4 text-4xl hover:text-[#00df9a] font-bold font-signature border-b border-b-green-500 '>Sudhanshu Sathawane</h1>
         <ul className='flex'>
-            <li className='p-4 border-b border-orange-500 hover:animate-bounce'><a href="https://github.com/Sudhanshu10Sathawane" target="_blank">Github</a></li>
-            <li className='p-4 border-b border-orange-500 hover:animate-bounce'><a href="https://www.linkedin.com/in/sudhanshu-sathawane/" target="_blank">Linkedin</a></li>
-            <li className='p-4 border-b border-orange-500 hover:animate-bounce'><a href="https://leetcode.com/furious10/" target="_blank">Leetcode</a></li>
-            <li className='p-4 border-b border-orange-500 hover:animate-bounce'><a href="https://codeforces.com/profile/furious10" target="_blank">Codeforces</a></li>
+            <li className='p-4 border-b border-orange-500 hover:scale-150 duration-200'><a href="https://github.com/Sudhanshu10Sathawane" target="_blank" rel="noreferrer">Github</a></li>
+            <li className='p-4 border-b border-orange-500 hover:scale-150 duration-200'><a href="https://www.linkedin.com/in/sudhanshu-sathawane/" target="_blank" rel="noreferrer">Linkedin</a></li>
+            <li className='p-4 border-b border-orange-500 hover:scale-150 duration-200'><a href="https://leetcode.com/furious10/" target="_blank" rel="noreferrer">Leetcode</a></li>
+            <li className='p-4 border-b border-orange-500 hover:scale-150 duration-200'><a href="https://codeforces.com/profile/furious10" target="_blank" rel="noreferrer">Codeforces</a></li>
         </ul>
     </div>
   );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React from 'react';
+import node from '../image/node.png';
+import css from '../image/css.png';
+import html from '../image/html.png';
+import github from '../image/github.png';
+import reactImage from '../image/react.png';
+import tailwind from '../image/tailwind.png';
+
+
 
 const Skills = () => {
+  const techs=[
+    {
+      id:1,
+      src:html,
+      title:'HTML',
+      style:'shadow-orange-500'
+    },
+    {
+      id:2,
+      src:css,
+      title:'CSS',
+      style:'shadow-blue-500'
+    },
+    {
+      id:3,
+      src:node,
+      title:'NODE.JS',
+      style:'shadow-green-500'
+    },
+    {
+      id:4,
+      src:reactImage,
+      title:'REACT',
+      style:'shadow-blue-600'
+    },
+    {
+      id:5,
+      src:github,
+      title:'GITHUB',
+      style:'shadow-white'
+    },
+    {
+      id:6,
+      src:tailwind,
+      title:'TAILWIND',
+      style:'shadow-sky-500'
+    }
+  ];
   return (
-    <div className='text-red-500'>Skills</div>
+    <div className='bg-gradient-to-t from-gray-800 to-black w-full h-screen'>
+      <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
+        <div>
+          <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Skills</p>
+          <p className='py-6'>Technologies I worked with</p>
+        </div>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+          {
+            techs.map(({id,src,title,style})=>(
+              <div key={id} className={`shadow-md hover:scale-105 duration-500 pxy-2 rounded-lg ${style}`}>
+            <img src={src} alt={src} className='w-20 mx-auto'/>
+            <p className='mt-4'>{title}</p>
+            </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
