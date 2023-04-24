bea6cf8f08aee49704959de322e277e7d5a96979
sudhanshu10sathawane <sudhanshusathwane@gmail.com>
2023-04-25 01:57:57 +0530

    Contact form created "Non working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Education from './components/Education';
 import Skills from './components/Skills';
+import Contact from './components/Contact';
 function App() {
   return (
   <>
     <Navbar/>
     <Education/>
     <Skills/>
+    <Contact/>
  </>
   );
 }
